7fdbf301a874c79e9e7eaeab7abe2ec294868fec
kamivision <kamivision@gmail.com>
2026-05-11 16:10:16 -0500

    updated logo

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-import CheckIn from "../components/widgets/CheckIn";
-import Vocab from "../components/Vocab";
-import DailyLinks from "../components/widgets/DailyLinks";
-=======
-import tempLogo from '../assets/temp_logo.png';
->>>>>>> dab7715 (updated styling and fixed token error in google auth)
+import logo from '../assets/compass_logo.svg';
 
 
 const HomePage = () => {
 
-<<<<<<< HEAD
-  return (
-    <>
-      <h1>hi i'm the homepage</h1>
-      <Vocab /> 
-      <CheckIn url="https://example.com" />
-      <DailyLinks url="/api/links" />
-    </>
-  );
-};
-=======
     return (
         <>
             <section className="hero">
                 <div className="hero-content">
-                    <img src={tempLogo} alt="COMPASS logo" className="mx-auto mb-8 w-75 max-w-md" />
+                    <img src={logo} alt="COMPASS logo" className="mx-auto mb-8 w-75 max-w-md" />
                     <h1 className="hero-title">Welcome to COMPASS</h1>
                     <p className="hero-description">Your all-in-one tool for navigating the Code Platoon curriculum and community.</p>
                 </div>
@@ -39,6 +22,5 @@ const HomePage = () => {
         </>
     );
 }
->>>>>>> dab7715 (updated styling and fixed token error in google auth)
 
 export default HomePage;
