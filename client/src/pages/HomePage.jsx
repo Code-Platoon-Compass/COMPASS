dab77154372178a4ff7de4ed317a73ce1422be92
kamivision <kamivision@gmail.com>
2026-05-11 14:07:45 -0500

    updated styling and fixed token error in google auth

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,22 @@
-import CheckIn from "../components/widgets/CheckIn";
-import Vocab from "../components/Vocab";
+import tempLogo from '../assets/temp_logo.png';
 
 
 const HomePage = () => {
 
     return (
         <>
+            <section className="hero">
+                <div className="hero-content">
+                    <img src={tempLogo} alt="COMPASS logo" className="mx-auto mb-8 w-75 max-w-md" />
+                    <h1 className="hero-title">Welcome to COMPASS</h1>
+                    <p className="hero-description">Your all-in-one tool for navigating the Code Platoon curriculum and community.</p>
+                </div>
+                <div>
+                    <button className="btn-primary" onClick={() => window.location.href = '/auth'}>Get Started</button>
+                </div>
+            </section>
             <h1>hi i'm the homepage</h1>
-            <Vocab /> 
-            <CheckIn url="https://example.com" />
+            
             
             
         </>
