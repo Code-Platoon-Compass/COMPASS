aa04f1a1cd720890c712de5129135d36e95edde1
Elia Cohen <mrc6113@rit.edu>
2026-05-12 12:47:05 -0400

    basic dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,15 +2,23 @@ import CheckIn from "../components/widgets/CheckIn";
 import Vocab from "../components/Vocab";
 import LogoutButton from '../components/LogoutButton';
 import TimezoneClock from '../components/TimezoneClock';
+import DailyLinks from "../components/widgets/DailyLinks";
+import { useOutletContext } from "react-router-dom";
 
 export default function Dashboard() {
+    const { user } = useOutletContext();
+
+    console.log(user);
+
     return (
         <>
+            { user && <h1>Welcome, {user.name.split(" ")[0]}</h1> }
             <h1>hi i'm the dashboard</h1>
             <Vocab /> 
             <CheckIn url="https://example.com" />
             <TimezoneClock />
             <LogoutButton />
+            { user && <DailyLinks url={`/api/v1/cohorts/${user["cohort_id"]}/daily-links`} />}
         </>
     )
 }
