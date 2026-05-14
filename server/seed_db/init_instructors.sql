6215a4214477a7d8f9580870d5f004f860ac136c
Tiffany A <tifalsuk@gmail.com>
2026-05-14 18:33:19 +0300

    Update init_instructors.sql
    script now prints API key

--- server/seed_db/init_instructors.sql
+++ server/seed_db/init_instructors.sql
@@ -1,2 +1,3 @@
 -- psql -U username -d myDataBase -a -f init_instructors.sql
 INSERT INTO instructors(id, name, email, api_key) VALUES (gen_random_uuid(), 'main', 'fake@example.com', gen_random_uuid());
+SELECT api_key FROM instructors;
