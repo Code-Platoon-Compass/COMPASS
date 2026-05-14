9e8f1a904bbda23270678932860270851a2fee5b
Elia Cohen <mrc6113@rit.edu>
2026-05-14 16:30:24 -0400

    clean up unused files

--- server/compass_api/test_settings.py
+++
@@ -1,9 +0,0 @@
-from .settings import *
-
-# Use in-memory cache for tests (Redis not available in test environment).
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'unique-snowflake',
-    }
-}
--- server/compass_api/test_settings_auth_only.py
+++
@@ -1,4 +0,0 @@
-from .settings import *
-
-# Isolate URL resolution for auth tests to avoid unrelated app import errors.
-ROOT_URLCONF = 'auth_app.urls'
